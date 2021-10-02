35038abfb071c6323df67b59e0871119eecd59f1
Louis D'Hont <louis.dhont@student.odisee.be>
2021-10-02 17:54:46 +0200

    Added a reset button

--- Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainViewModel.cs
+++ Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainViewModel.cs
@@ -11,12 +11,6 @@ namespace MandelbrotFractalApplication.Presentation
 {
     public class MainViewModel : ObservableObject
     {
-        private readonly ILogic logic;
-
-        public WriteableBitmap BitmapDisplay { get; private set; }
-
-        public IRelayCommand CalculateCommand { get; private set; }
-
         private const int Width = 800;
         private const int Height = 800;
 
@@ -27,11 +21,25 @@ namespace MandelbrotFractalApplication.Presentation
 
         public int selectedIterations = 250;
 
+        public double zoomScale = 1;
+
+        public double xOffset = 0;
+        public double yOffset = 0;
+
         private bool working = false;
+
+        private readonly ILogic logic;
+
+        public WriteableBitmap BitmapDisplay { get; private set; }
+
+        public IRelayCommand CalculateCommand { get; private set; }
+
+        public IRelayCommand ResetCommand { get; private set; }
 
         public MainViewModel(ILogic logic)
         {
             this.logic = logic;
+            ResetCommand = new RelayCommand(() => ResetMandelbrot(), () => !working);
             CalculateCommand = new RelayCommand(async () => await MandelbrotAsync(), () => !working);
             CreateBitmap(MaxColumn, MaxRow);
         }
@@ -43,6 +51,14 @@ namespace MandelbrotFractalApplication.Presentation
             var pixelFormat = PixelFormats.Pbgra32;
             BitmapDisplay = new WriteableBitmap(width, height, dpiX, dpiY, pixelFormat, null);
             OnPropertyChanged(nameof(BitmapDisplay));
+        }
+
+        private void ResetMandelbrot()
+        {
+            zoomScale = 1;
+            xOffset = 0;
+            yOffset = 0;
+            CalculateCommand.Execute(null);
         }
 
         private async Task MandelbrotAsync()
@@ -64,8 +80,8 @@ namespace MandelbrotFractalApplication.Presentation
             {
                 Parallel.For(0, Width, y =>
                 {
-                    double a = (Width / 2 - x) * scale;
-                    double b = (y - Height / 2) * scale;
+                    double a = (Width / 2 - x) * scale / zoomScale + xOffset;
+                    double b = (y - Height / 2) * scale / zoomScale + yOffset;
                     int mutationsDepth = logic.CalcMandelbrotDepth(new ComplexNumber(b, a), selectedIterations);
 
                     var color = GetColor(mutationsDepth);
--- Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainWindow.xaml.cs
+++ Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ namespace MandelbrotFractalApplication.Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
-
         private readonly MainViewModel viewModel;
 
         private readonly int[] Iterations = { 50, 100, 250, 500, 1000, 2000, 5000 };
@@ -39,6 +38,29 @@ namespace MandelbrotFractalApplication.Presentation
             }
             viewModel.selectedIterations = (int)IterationsCbx.SelectedItem;
             if (viewModel.CalculateCommand.CanExecute(null)) viewModel.CalculateCommand.Execute(null);
+        }
+
+        private void MdbImage_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                viewModel.zoomScale *= 1.4d;
+                var pos = e.GetPosition(this);
+
+                viewModel.xOffset = (pos.X / 400) / viewModel.zoomScale;
+                viewModel.yOffset = (pos.Y / 400) / viewModel.zoomScale;
+                if (viewModel.CalculateCommand.CanExecute(null)) viewModel.CalculateCommand.Execute(null);
+            }
+            else if (e.Delta < 0)
+            {
+                viewModel.zoomScale /= 1.4d;
+                if (viewModel.CalculateCommand.CanExecute(null)) viewModel.CalculateCommand.Execute(null);
+            }
+        }
+
+        private void ResetMandelBrot_Click(object sender, RoutedEventArgs e)
+        {
+            if (viewModel.ResetCommand.CanExecute(null)) viewModel.ResetCommand.Execute(null);
         }
     }
 }
