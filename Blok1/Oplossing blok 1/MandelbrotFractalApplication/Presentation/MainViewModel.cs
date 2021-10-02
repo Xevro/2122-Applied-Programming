6b67ed29a74f5fdaea3086b3ce5e266fc2d60c9a
Louis D'Hont <louis.dhont@student.odisee.be>
2021-10-02 17:13:16 +0200

    Optimized the parallel for loop

--- Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainViewModel.cs
+++ Blok1/Oplossing blok 1/MandelbrotFractalApplication/Presentation/MainViewModel.cs
@@ -47,45 +47,31 @@ namespace MandelbrotFractalApplication.Presentation
         {
             working = true;
             CalculateCommand.NotifyCanExecuteChanged();
-            await GenerateBitmap();
+            GenerateBitmap();
             working = false;
             CalculateCommand.NotifyCanExecuteChanged();
         }
 
-        private async Task GenerateBitmap()
+        private void GenerateBitmap()
         {
             var pixelBlock = new Color[Height, Width];
             double scale = MaxValueExtent / Math.Min(Width, Height);
+            uint[,] pixels = new uint[Height, Width];
 
-            for (int x = 0; x < Width; x++)
+            Parallel.For(0, Height, x =>
             {
-                for (int y = 0; y < Width; y++)
+                Parallel.For(0, Width, y =>
                 {
                     double a = (Width / 2 - x) * scale;
                     double b = (y - Height / 2) * scale;
                     int mutations = logic.CalcMandelbrotDepth(new ComplexNumber(b, a), 250);
 
-                    pixelBlock[x, y] = GetColor(mutations);
-                };
-            };
-            SetBlock(pixelBlock, 0, 0);
-        }
-
-        private void SetBlock(Color[,] colors, int startRow, int startColumn)
-        {
-            int numberOfRows = colors.GetLength(1);
-            int numberOfColumns = colors.GetLength(0);
-            uint[,] pixels = new uint[numberOfColumns, numberOfRows];
-            for (int row = 0; row < numberOfRows; row++)
-            {
-                for (int column = 0; column < numberOfColumns; column++)
-                {
-                    var color = colors[column, row];
-                    pixels[column, row] = BitConverter.ToUInt32(new byte[] { color.B, color.G, color.R, color.A });
-                }
-            }
-            var rectangle = new Int32Rect(0, 0, numberOfColumns, numberOfRows);
-            BitmapDisplay.WritePixels(rectangle, pixels, BitmapDisplay.BackBufferStride, startColumn, startRow);
+                    var color = GetColor(mutations);
+                    pixels[x, y] = BitConverter.ToUInt32(new byte[] { color.B, color.G, color.R, color.A });
+                });
+            });
+            var rectangle = new Int32Rect(0, 0, Height, Width);
+            BitmapDisplay.WritePixels(rectangle, pixels, BitmapDisplay.BackBufferStride, 0, 0);
         }
 
         public static Color GetColor(int value)
